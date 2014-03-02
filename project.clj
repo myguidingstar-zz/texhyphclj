754bdc620d5a781933df8c10d0f2fedc1c492fa1
Hoàng Minh Thắng <p@banphim.net>
2014-03-02 22:58:45 +0700

    add project.clj

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject texhyphclj "0.1.0-SNAPSHOT"
+  :description "Clojure wrapper for Texhyphj, a Java Hyphenation library."
+  :url "http://github.com/myguidingstar/texhyphclj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.googlecode.texhyphj/texhyphj "1.2"]])
